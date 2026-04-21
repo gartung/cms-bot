ffa6113f0d19cdfc57bda0909e5563c352bb38a8
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-04-21 09:32:33 +0200

    Add 'rocm' to forward_ports_map for cmsdist

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -148,6 +148,7 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"] = []
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/rootmaster")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/root638")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/fp")
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/rocm")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/clang")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/devel")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_17_0_X/g4")
